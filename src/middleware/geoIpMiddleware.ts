bdf7523c18c1f4ea3c9b3654cd09513ffdbf4c7e
Mik ten Holt <miktenholt@gmail.com>
2025-02-14 23:30:33 +0100

    Remove commented-out code and streamline geoIpMiddleware function

--- src/middleware/geoIpMiddleware.ts
+++ src/middleware/geoIpMiddleware.ts
@@ -3,9 +3,7 @@ import geoip from 'geoip-lite'
 
 export function geoIpMiddleware(req: Request, res: Response, next: NextFunction) {
     const ip = req.ip ?? ''
-    // const ip = `80.61.194.72`
     req.geo = geoip.lookup(ip)
-    console.log('geoIpMiddleware', req.geo)
 
     next()
 }
